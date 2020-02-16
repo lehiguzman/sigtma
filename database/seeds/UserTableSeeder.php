87e922d3ddd92356806356340284e13adc902a43
Lehi Guzman <lguzman@strappinc.com>
2020-02-16 19:39:45 -0400

    commit inicial

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@correo.com';
+		$user->password = bcrypt('admin');
+        $user->save();
+    }
+}
