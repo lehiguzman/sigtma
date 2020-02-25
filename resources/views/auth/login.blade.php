fd8830091585d9e5e9251e19441ef88f7dedc1f7
Lehi Guzman <lehyjoseph2007@gmail.com>
2020-02-25 11:11:37 -0600

    Cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-5 col-form-label text-md-right"><h4><i>{{ __('E-Mail Address') }}</i></h4></label>
+                            <label for="email" class="col-md-5 col-form-label text-md-right"><h5><i>{{ __('E-Mail Address') }}</i></h5></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -27,7 +27,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-5 col-form-label text-md-right"><h4><i>{{ __('Password') }}</i></h4></label>
+                            <label for="password" class="col-md-5 col-form-label text-md-right"><h5><i>{{ __('Password') }}</i></h5></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right"><h4><i>{{ __('Name') }} completo</i></h4></label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right"><h5><i>{{ __('Name') }} completo</i></h5></label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -27,7 +27,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right"><h4><i>{{ __('E-Mail Address') }}</i></h4></label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right"><h5><i>{{ __('E-Mail Address') }}</i></h5></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -41,7 +41,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right"><h4><i>{{ __('Password') }}</i></h4></label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right"><h5><i>{{ __('Password') }}</i></h5></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -55,7 +55,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right"><h4><i>{{ __('Confirm Password') }}</i></h4></label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right"><h5><i>{{ __('Confirm Password') }}</i></h5></label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
